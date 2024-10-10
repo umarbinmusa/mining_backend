69749c561f110c57869281a44e6028be845ea841
umarbinmusa <umarmusa20002@gmail.com>
2024-10-10 12:07:46 +0100

    n

--- client/src/Mine.tsx
+++ client/src/Mine.tsx
@@ -1,11 +1,14 @@
-
 import Friends from './icons/Friends';
 import Coins from './icons/Coins';
 import { hamsterCoin } from './images';
 import { useQuery } from '@apollo/client';
 import { GET_TASKS_QUERY } from "./Queries"; 
-import { Navigate } from "react-router-dom";
 
+interface Task {
+  id: string;
+  url: string;
+  verify: string; // adjust types as necessary
+}
 
 const Mine = () => {
   const { loading, error, data } = useQuery(GET_TASKS_QUERY);
@@ -17,60 +20,35 @@ const Mine = () => {
     <div className="bg-black flex justify-center">
       <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
         <div className="px-4 z-10">
-          <div className="flex items-center space-x-2 pt-4">
-            <div className="p-1 rounded-lg bg-[#1d2025]"></div>
-          </div>
-          <div className="flex items-center justify-between space-x-4 mt-1">
-            <div className="flex items-center w-1/3">
-              <div className="w-full">
-                <div className="flex items-center mt-1 border-2 border-[#43433b] rounded-full">
-                  <div className="w-full h-2 bg-[#43433b]/[0.6] rounded-full">
-                    <div className="progress-gradient h-2 rounded-full"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {/* Header Content */}
         </div>
 
         <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
           <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
             <div className="px-4 mt-6 flex justify-between gap-2">
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative"></div>
-
-             
             </div>
 
             <div className="px-4 mt-4 flex justify-center"></div>
             <div>
-    
-                <ul className=''>
-                  {data.getTasks.map((task) => (
-                    <li key={task.id}>
-                     <a href="/www.youtube.com" target='blank' rel="noopener noreferrer"> <strong>URL:</strong> {task.url} <br /> </a>
-                      <strong>Verify:</strong> {task.verify}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <ul>
+                {data.getTasks.map((task: Task) => (
+                  <li key={task.id}>
+                    <a href="/www.youtube.com" target='blank' rel="noopener noreferrer">
+                      <strong>URL:</strong> {task.url} <br />
+                    </a>
+                    <strong>Verify:</strong> {task.verify}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Bottom fixed div */}
       <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
-        <div className="text-center text-[#85827d] w-1/5">
-          <Friends className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Friends</p>
-        </div>
-        <div className="text-center text-[#85827d] w-1/5">
-          <Coins className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Home</p>
-        </div>
-        <div className="text-center text-[#85827d] w-1/5">
-          <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Airdrop</p>
-        </div>
+        {/* Bottom Nav Content */}
       </div>
     </div>
   );
